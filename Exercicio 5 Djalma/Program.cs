51f660dec9c503cbcc888516cee743dcf5051744
Vinícius <Vincius@DESKTOP-HLF9SUT>
2022-07-06 19:42:49 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 5 Djalma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_5_Djalma
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int i;
+			for (i = 1; i <= 100 / 2; i++)
+			{
+				Console.WriteLine(i * 1);
+			}
+			Console.ReadLine();
+		}
+	}
+}
